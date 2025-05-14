1136f54ad760472b089d4a7d4f64e70da01b05d0
Leandro Resende <resendeleandro565@gmail.com>
2025-05-14 01:42:39 +0100

    Added create service  page first css version, with small problems but working

--- templates/create_service_body.php
+++ templates/create_service_body.php
@@ -6,40 +6,38 @@ function draw_create_service_body()
   <main>
     <h2>Create a Service</h2>
     <form id="create-service-form" method="POST" enctype="multipart/form-data" action="../actions/action_create_service.php">
-      <div class="form-line">
-        <label for="service-title">Service Title:</label>
-        <input type="text" id="service-title" name="service_title" required>
+      <div id="form-title">
+        <label for="service-title">Service Title</label>
+        <input type="text" id="service-title" name="service_title" placeholder="Title" required>
       </div>
 
-      <div class="form-line">
-        <label for="service-description">Description:</label>
-        <textarea id="service-description" name="service_description" required></textarea>
+      <div id="form-description">
+        <label for="service-description">Description</label>
+        <textarea id="service-description" name="service_description" placeholder="Description" required></textarea>
       </div>
 
-      <div class="form-line">
-        <label for="service-price">Price:</label>
-        <input type="number" id="service-price" name="service_price" required>
+      <div id="form-location">
+        <label for="service-location">Location</label>
+        <input type="text" id="service-location" name="service_location" placeholder="Location" required>
       </div>
 
-      <div class="form-line">
-        <label for="service-location">Location:</label>
-        <input type="text" id="service-location" name="service_location" required>
+      <div id="form-price">
+        <label for="service-price">Price</label> 
+        <input type="number" id="service-price" name="service_price" min="0" max="1000" placeholder="Price" required>
+        <input type="range" id="price-slider" min="0" max="1000" value="0">
       </div>
 
-      <div class="form-line">
-        <label for="service-category">Category:</label>
-        <select id="service-category" name="service_category" required>
-          <!-- Categories will be dynamically loaded here -->
-        </select>
+      <div id="form-category">
+        <!-- Categories buttons will be dynamically loaded here --> 
       </div>
 
-      <div class="form-line">
-        <label for="service-image">Service Image:</label>
-        <input type="file" id="service-image" name="service_image" required>
+      <div id="form-image">
+        <label for="service-image">Service Image</label>
+        <input type="file" id="service-image" name="service_image">
       </div>
 
       <button type="submit">Create Service</button>
-    </form>  
+    </form> 
   </main>
   <?php
 }
