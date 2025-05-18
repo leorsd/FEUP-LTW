d40a6c870116998c8d95c03778ae0f94fd1da719
snaldos <arnaldoflopes@gmail.com>
2025-05-18 21:57:49 +0100

    the sessions starts() are needed

--- actions/action_change_password.php
+++ actions/action_change_password.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+session_start();
 require_once(__DIR__ . '/../includes/db/connection.php');
 require_once(__DIR__ . '/../lib/user.php');
 
--- actions/action_logout.php
+++ actions/action_logout.php
@@ -1,7 +1,13 @@
 <?php
 declare(strict_types=1);
 
+session_start();
+
+// Unset all session variables
+$_SESSION = [];
+
 session_destroy();
+
 header('Location: ../pages/welcome.php');
 
 exit();
